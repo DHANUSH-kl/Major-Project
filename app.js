fd6c693f30851b0dc899c62f17aa3f9f08226e56
Dhanush KL <dhanushchandu1232@gmail.com>
2024-01-13 22:46:35 +0530

    error handling

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const Listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride=require("method-override")
 const ejsMate= require('ejs-mate');
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema}= require("./schema.js");
 
 
 app.set("view engine","ejs");
@@ -20,13 +23,10 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
-app.listen(port,(req,res)=>{
-    console.log(`Server is listening to the post: ${port}`);
-})
-
 app.get("/",(req,res)=>{
     res.send("Welcome to the root page");
 })
+
 
 // app.get("/testlistings",async (req,res)=>{
 //     // let sampleListing= new Listing({
@@ -43,43 +43,69 @@ app.get("/",(req,res)=>{
 //     // res.send("saved successfully")
 // });
 
+const validateListing=(req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(404,error);
+    }else{
+        next()
+    };
+}
 
-app.get("/listings",async (req,res)=>{
+app.get("/listings",wrapAsync(async (req,res)=>{
     let allListings=await Listing.find();
     res.render("listings/index.ejs",{allListings});
-})
+}));
 
 app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs")
 })
 
-app.get("/listings/:id/edit",async(req,res)=> {
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=> {
     let {id}=req.params;
     let listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{listing})
 
-})
+}));
 
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id", validateListing ,wrapAsync(async(req,res)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"enter valid data for listing")
+    }
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing})
     res.redirect(`/listings/${id}`)
-})
+}));
 
-app.post("/listings",async(req,res)=>{
-   const newListing= new Listing(req.body.listing)
-   await newListing.save();
-   res.redirect("/listings")
-})
+app.post("/listings",validateListing ,wrapAsync(async(req,res)=>{
+    const newListing= new Listing(req.body.listing)
+    await newListing.save();
+    res.redirect("/listings")
+    }));
 
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let deleted= await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-})
+}));
 
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let listings = await Listing.findById(id);
     res.render("listings/show.ejs",{listings});
+}));
+
+app.all("*",(req,res,next)=> {
+    next(new ExpressError(404,"page not found"));
+});
+
+app.use((err,req,res,next)=>{
+    let {statusCode=500,message="something went wrong"}=err;
+    res.status(statusCode).render("listings/error.ejs",{message});
+    // res.status(statusCode).send(message);
+})
+
+app.listen(port,(req,res)=>{
+    console.log(`Server is listening to the post: ${port}`);
 })
--- models/listing.js
+++ models/listing.js
@@ -7,12 +7,11 @@ const listingSchema = new Schema({
         required:true
     },
     description:String,
-    image:({
-        default:"https://images.unsplash.com/photo-1600585154340-be6161a56a0c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dmlsbGF8ZW58MHx8MHx8fDA%3D",
-        type:String,
-        set:(v)=>(v===""?"https://images.unsplash.com/photo-1600585154340-be6161a56a0c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dmlsbGF8ZW58MHx8MHx8fDA%3D"
-            :v)
-    }),
+    image: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dmlsbGF8ZW58MHx8MHx8fDA%3D",
+        set: (v) => (v === "" ? "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dmlsbGF8ZW58MHx8MHx8fDA%3D" : v)
+    },
     price:String,
     location:String,
     country:String
