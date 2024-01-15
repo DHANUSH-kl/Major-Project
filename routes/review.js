7602687d67fbae39963cc7107b0162da96205533
Dhanush KL <dhanushchandu1232@gmail.com>
2024-01-15 18:07:11 +0530

    resolved listing error

--- routes/review.js
+++ routes/review.js
@@ -4,6 +4,8 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const { reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
+const Listing = require("../models/listing.js");
+
 
 
 
